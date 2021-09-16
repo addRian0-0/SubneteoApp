a162e955ab4e9b8aaae7cb61ec0fbff8351bb98b
addRian0-0 <addrianun@gmail.com>
2021-09-16 13:03:06 -0500

    resolve crossorigin error?

--- pages/subneteo1.jsx
+++ pages/subneteo1.jsx
@@ -122,7 +122,7 @@ export default function Home() {
         <div>
             <Head>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
-                    integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous" />
+                    integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"  />
                 <title>Programa de subneteo</title>
             </Head>
 
